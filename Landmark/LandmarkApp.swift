2c783d465c713d74a54221cf547c994915095846
Hidemitsu Shmizu <hide0917>
2021-09-12 02:58:20 +0900

    first commit

---
+++ Landmark/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by Hidemitsu Shmizu on 2021/09/12.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
