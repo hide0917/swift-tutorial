57389ddb810355d7dd15a88e027933a705245bc8
Hidemitsu Shmizu <hide0917>
2021-09-23 20:18:58 +0900

    Drawing Paths and Shapes

---
+++ Landmark/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmark
+//
+//  Created by Hidemitsu Shmizu on 2021/09/23.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
